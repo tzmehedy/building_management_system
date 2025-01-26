e1abc60ae272cdd7e0567aa2d5392ba4060561a1
unknown <mehedytouhidurzaman@gmail.com>
2025-01-26 20:39:07 +0600

    The basic setup of server and create a route for user is completed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000
 
@@ -10,6 +12,41 @@ app.use(express.json())
 app.get("/", async(req,res)=>{
     res.send("The building management data will come soon")
 })
+
+
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}}@cluster0.k8aq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("BuildingManagementSystem").collection("allUsers")
+
+    app.patch("/users", async(req,res)=>{
+        const userInfo = req.body
+        const result = await usersCollection.insertOne(userInfo)
+        res.send(result)
+    })
+    
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, ()=>{
     console.log(`The server will run on the port of ${port}`)
