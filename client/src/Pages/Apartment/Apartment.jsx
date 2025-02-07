518ba306aaa3b6efdbf72aed02f9295a06ebc910
unknown <mehedytouhidurzaman@gmail.com>
2025-02-08 01:49:08 +0600

    All aparments data get using tanstack query is completed

--- client/src/Pages/Apartment/Apartment.jsx
+++ client/src/Pages/Apartment/Apartment.jsx
@@ -1,126 +1,28 @@
-
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 const Apartment = () => {
+  const axiosPublic = useAxiosPublic()
+
+  const {data:allApartments=[]} = useQuery({
+    queryKey: ["allApartments"],
+    queryFn: async () =>{
+      const {data} = await axiosPublic.get("/allApartments")
+      return data
+    }
+  })
+
+  console.log(allApartments)
+  
+
     return (
       <div className="mt-40">
-        <h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
-          ratione suscipit consectetur distinctio quae sit praesentium
-          voluptatibus dolor doloremque quibusdam provident, iure magni tenetur
-          cumque in mollitia necessitatibus delectus. Deleniti dicta ipsa,
-          temporibus possimus placeat inventore consequuntur? Aspernatur at
-          temporibus molestiae quod, sapiente earum? Eum obcaecati repudiandae
-          voluptatibus incidunt voluptates molestias odio itaque, officia omnis
-          illo sapiente quibusdam quidem facere vitae totam. Eveniet esse
-          voluptatibus veniam nihil, neque in suscipit a tempore exercitationem
-          ipsam adipisci labore, aspernatur enim! Dignissimos soluta magni iusto
-          laborum impedit maiores cum eum! Animi earum provident neque illo,
-          fugiat numquam quam labore maxime quos ex repellat ullam, veritatis
-          beatae ipsa necessitatibus expedita! Veniam beatae, atque non
-          cupiditate, dignissimos sint rerum illum officia est facere
-          reiciendis? Tenetur voluptates suscipit earum, officia magnam iusto
-          eveniet minus qui nobis fugiat quibusdam temporibus quaerat vero
-          placeat laboriosam at reiciendis architecto aut? Repudiandae non a
-          quasi delectus iste itaque saepe odit necessitatibus ducimus, ut
-          dolore explicabo consectetur quidem nesciunt aperiam aspernatur id
-          reiciendis. Vitae harum, assumenda vero a voluptatibus totam rerum,
-          nulla ipsam sapiente non reiciendis ipsum dicta cupiditate architecto
-          sed inventore libero asperiores facilis deleniti ad delectus mollitia.
-          Fugit inventore totam sapiente dolorum magnam, quaerat provident
-          corporis dolore laboriosam incidunt debitis culpa sunt asperiores quam
-          quas eum ratione id aperiam? Hic cumque ex dignissimos, quibusdam modi
-          doloremque quis veniam error numquam officiis ratione asperiores illum
-          iusto provident fugit commodi consectetur quaerat reprehenderit
-          repellat cupiditate officia, perspiciatis dolor. Molestiae officiis ab
-          voluptate nesciunt ducimus, reprehenderit eveniet facilis, deleniti
-          voluptatum ipsa vitae corrupti, eaque alias dolor deserunt doloremque!
-          Quis libero veritatis laborum autem odio. Omnis dolore enim esse iure
-          atque voluptatum repudiandae minus at debitis ea, earum quaerat
-          reprehenderit? Praesentium nemo excepturi, ut suscipit repudiandae
-          harum impedit consequatur deserunt? Atque sit vitae incidunt maxime
-          officia officiis assumenda ad laborum, impedit sequi similique
-          perspiciatis quaerat dignissimos at, sapiente maiores alias doloremque
-          eveniet hic amet reprehenderit cum laboriosam! Beatae ratione cum
-          earum minima asperiores officia facere cupiditate quasi dolores saepe
-          alias corporis, repellendus dicta labore veniam voluptate sit
-          exercitationem odio unde architecto, magni iusto! Quia sed cum
-          corporis doloribus maxime placeat nulla asperiores aliquam, laboriosam
-          quisquam a, cupiditate recusandae, debitis ex molestiae facere ut
-          officiis aspernatur illum veritatis minus nemo dolore? Dignissimos
-          facilis rem beatae, vero obcaecati est eum fugiat quae, officia alias
-          omnis accusamus repellat voluptatum repellendus fugit quia dolore
-          deleniti laudantium suscipit neque itaque quis. Minus voluptates sint
-          ipsa laudantium? Debitis dolore similique aliquid officia, officiis
-          eligendi delectus sit corporis! Aperiam dicta possimus expedita
-          impedit quas nemo aut fuga! Maxime expedita numquam dolore sequi quia
-          facilis aperiam, mollitia enim? Quae magni quas officia quos magnam.
-          Velit, modi voluptate. Fugit amet in excepturi impedit ullam quia
-          libero. Cupiditate illo adipisci omnis exercitationem alias vel
-          voluptatibus cumque, recusandae quibusdam expedita. Inventore quae
-          numquam modi sit suscipit rerum! Voluptatibus quas praesentium nihil
-          hic animi provident, voluptate, in corporis quis assumenda id! Vero id
-          corrupti rerum delectus placeat. Quaerat voluptate optio hic
-          cupiditate quam beatae omnis commodi quos vel voluptas velit libero,
-          tempore at. Qui numquam, velit totam aliquid quidem assumenda
-          aspernatur, maxime ipsum eligendi repudiandae praesentium? Dolores
-          distinctio unde voluptatum odio doloremque atque dignissimos eligendi
-          ab in sequi fuga cum, ex adipisci? At cupiditate deleniti inventore
-          deserunt officiis provident numquam, possimus nesciunt magni nisi
-          tempore suscipit sapiente facilis laboriosam doloribus! Aut dolorem
-          molestias unde maiores soluta architecto ipsum, doloremque ducimus
-          fugit ad voluptate aperiam illo? Ab velit architecto eaque voluptatem
-          explicabo assumenda quam consectetur aliquam aliquid nisi, dolorem
-          ratione dignissimos dolor nesciunt. Voluptas aspernatur eius odit
-          beatae ut ipsa, autem nam facilis libero tenetur aliquid, accusamus
-          dolor aut. Ratione, fuga! Sed libero, totam hic fugit consequatur
-          vitae perferendis officia enim, qui aperiam nostrum eum explicabo iure
-          beatae magnam doloremque, illum accusantium maiores. Expedita fugiat
-          repudiandae at voluptatibus aut earum cupiditate. Iure ea, pariatur
-          eum, nostrum harum officia iusto autem perspiciatis illum, molestias
-          esse quis nobis sit. Esse alias nesciunt ex odio voluptate, possimus
-          sunt harum iure voluptates nobis neque nostrum consequatur quis
-          officiis quasi ipsa consectetur, illo omnis. Reprehenderit velit eaque
-          quam, laboriosam quod corrupti, libero commodi maiores molestias
-          repellat fugit eligendi expedita voluptatum delectus dolores! Saepe
-          voluptatibus voluptas quibusdam praesentium rem aut recusandae? Enim
-          inventore vel aliquid id quidem et assumenda. Quia totam
-          necessitatibus culpa accusamus voluptatibus molestias odit tenetur? In
-          nihil distinctio sapiente voluptatum, reprehenderit nemo eligendi?
-          Quia ducimus, ad laborum, odio magnam commodi quidem magni eveniet
-          totam impedit, animi voluptates tempore provident deserunt aperiam hic
-          debitis dolor possimus aut id. Tenetur a dolorum quidem eius
-          doloremque nam quibusdam, officiis molestias, facilis fuga minus
-          repellat suscipit. Eum ipsam expedita voluptate voluptatum dolores
-          quisquam quo? Aspernatur eveniet repellendus quod inventore officiis.
-          Animi repellendus nihil ab corporis consequatur, nobis sunt recusandae
-          exercitationem doloremque saepe non numquam porro neque dicta aliquam
-          deserunt sint nesciunt, minima fugit vitae explicabo reiciendis
-          adipisci molestiae id? Sint ab nobis officia exercitationem corporis
-          rem culpa libero eius porro. Odit velit laudantium, sapiente maxime
-          laboriosam veritatis suscipit quod mollitia autem illum porro
-          accusamus, ipsum totam ratione tempore. Inventore rerum nisi tempore
-          sapiente fuga harum voluptatum? Provident, repellendus in error
-          deleniti doloremque deserunt tempore laborum asperiores consequatur
-          rerum repellat molestiae mollitia magni laudantium, enim cupiditate
-          aut eum quaerat minima. Deserunt odit rerum asperiores atque repellat
-          porro velit, veniam modi. Vel fugiat, architecto totam quis placeat
-          deserunt dolorum vero suscipit sunt quia exercitationem omnis illum
-          enim iusto, quidem, nesciunt fugit perspiciatis ab libero! Tempora
-          deleniti perspiciatis delectus, harum neque, officia magnam cumque
-          quis mollitia omnis corrupti sed repellat excepturi consequuntur
-          quisquam quaerat culpa ducimus? Officiis illum magni excepturi in
-          tempore, molestiae neque aperiam alias repellat eligendi facere
-          recusandae atque itaque consectetur. Laudantium quis eos sunt, rerum
-          harum, qui vitae nihil ex nulla officiis at voluptates maxime enim a?
-          Voluptatibus eaque unde, accusantium veritatis laborum atque ab. Omnis
-          qui officiis ipsam ullam rerum velit voluptatum aliquid illum
-          excepturi assumenda cupiditate saepe ipsa molestias quod reprehenderit
-          nobis adipisci quo unde, quaerat facilis error sit? Quam architecto
-          itaque, earum consectetur alias vero temporibus! Assumenda, tempore
-          iste. Sint iure debitis nesciunt reiciendis. Atque laboriosam pariatur
-          voluptatibus veniam fugit.
-        </h1>
+       <div className="text-center">
+        <h1 className="text-4xl font-bold">All Apartments</h1>
+       </div>
+       <div>
+
+       </div>
       </div>
     );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Router'
-import AuthProvider from './Provider/AuthProvider'
-import { ToastContainer } from 'react-toastify'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Router";
+import AuthProvider from "./Provider/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-      <ToastContainer></ToastContainer>
-    </AuthProvider>
-  </StrictMode>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+        <ToastContainer></ToastContainer>
+      </AuthProvider>
+    </QueryClientProvider>
+  </>
 );
